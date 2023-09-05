b6a22653f49910e055e65d980d446373c126ff8f
Sebas <sebastianwilson03@hotmail.com>
2023-09-05 11:11:59 -0500

    CiclosJS

---
+++ CICLOS JS.js
@@ -0,0 +1,25 @@
+/*
+// DO WHILE
+var i = 0;
+do {
+    console.log(i);
+} while(i < 5);
+*/
+
+/*
+//For While
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
+*/
+
+/*
+//While
+var i = 0;
+while (i < 5) {
+    console.log(i);
+    i++
+}
+*/
+
+// SOLO LES QUETA LOS COMENTARIOS PARA EJECUTAR EL CODIGO //
